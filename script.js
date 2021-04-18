9035b0bbf8ff560c7b3478a64e69fb097ed926cc
randombodyingithub <82085080+randombodyingithub@users.noreply.github.com>
2021-04-18 12:16:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,29 @@ function discord() {
 function instagram() {
     window.open("https://www.instagram.com/alfaerkekteskilati/")
 }
+
+  function inferna() {
+    window.open("https://discord.com/users/395627532419858433")
+  }
+
+  function jenap() {
+    window.open("https://discord.com/users/710095299447881739 ")
+  }
+
+  document.getElementById("animate1").classList.add('animate__animated');
+  document.getElementById("animate1").classList.add('animate__slideInRight');
+
+  setTimeout(function() {
+  document.getElementById("animate2").classList.add('animate__animated');
+  document.getElementById("animate2").classList.add('animate__slideInRight');
+  },100)
+
+  setTimeout(function() {
+  document.getElementById("animate3").classList.add('animate__animated');
+  document.getElementById("animate3").classList.add('animate__slideInRight');
+  }, 200)
+
+  setTimeout(function() {
+  document.getElementById("animate4").classList.add('animate__animated');
+  document.getElementById("animate4").classList.add('animate__slideInRight');
+  }, 300)
